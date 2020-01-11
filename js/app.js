08879b244f97952855a46e9d279b942f1c9153f9
mdabdulraheem <mohdraheem68@gmail.com>
2020-01-12 01:18:11 +0530

    changes

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
  // fixed nav background 
  const nav = document.querySelector(".nav");
- if( window.scrollY >= 0 ) {
+ if( window.innerWidth > 768 ) {
      nav.style.background = "none";
  }
  
  function check() {
-
      if( window.scrollY + document.querySelector(".nav").getBoundingClientRect().top > 0 ) {
          nav.classList.add("navFixed");
      }
